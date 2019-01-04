4d18c53f325507149c6859bdcc15a2a50689af1a
ron bar yosef <ronibary@gmail.com>
2019-01-05 00:28:29 +0200

    adding comment on testingChrome file

--- src/main/java/com/ron/learning/testingChrome.java
+++ src/main/java/com/ron/learning/testingChrome.java
@@ -3,6 +3,7 @@ package com.ron.learning;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//testing chrome browser
 public class testingChrome {
 
 	public static void main(String[] args) 
