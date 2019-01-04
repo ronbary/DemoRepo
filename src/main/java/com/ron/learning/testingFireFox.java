f678668af36f8799ebb4d78797067c0901ef659e
ron bar yosef <ronibary@gmail.com>
2019-01-04 21:58:34 +0200

    My first commit of selenium project

---
+++ src/main/java/com/ron/learning/testingFireFox.java
@@ -0,0 +1,21 @@
+package com.ron.learning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class testingFireFox {
+
+	public static void main(String[] args) {
+		
+		
+		// in order to run FF with selenium 3 and above 
+		// need latest version of FF browser
+		
+		System.setProperty("webdriver.gecko.driver","D://Automation/Selenium/Drivers/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+	
+		driver.get("http://www.google.com");
+	
+	}
+
+}
---
+++ src/main/java/com/ron/util/TestUtil.java
@@ -0,0 +1,11 @@
+package com.ron.util;
+
+public class TestUtil {
+	
+	
+	public void MyPrintTest() {
+		System.out.println("testing print out happy new year !");
+	}
+	
+	
+}
